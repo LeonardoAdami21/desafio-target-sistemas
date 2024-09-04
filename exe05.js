c416b566efbabc8649f89bd5c89b23b9f3fe9e29
LeonardoAdami21 <lolzinhobr1000@gmail.com>
2024-09-04 10:49:48 -0300

    master, exercicio05 finalizado

---
+++ exe05.js
@@ -0,0 +1,5 @@
+
+// 5) Você está em uma sala com três interruptores, cada um conectado a uma lâmpada em salas diferentes. Você não pode ver as lâmpadas da sala em que está, mas pode ligar e desligar os interruptores quantas vezes quiser. Seu objetivo é descobrir qual interruptor controla qual lâmpada. Como você faria para descobrir, usando apenas duas idas até uma das salas das lâmpadas, qual interruptor controla cada lâmpada?  
+
+
+// Resposta: O interruptor 2 controla a lâmpada 1 e o interruptor 3 controla a lâmpada 2 
